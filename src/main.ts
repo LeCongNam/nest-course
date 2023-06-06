32716ba3d65e534871cc34e8a8798b2dcf417570
LeCongnam <namlem4u@gmail.com>
2023-06-07 02:04:45 +0700

    change example source and show port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
-  await app.listen(3000);
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+
+  if (process.env.NODE_ENV === 'development') {
+    console.log(`\x1b[32m Server start PORT: ${PORT}`);
+  }
 }
 bootstrap();
